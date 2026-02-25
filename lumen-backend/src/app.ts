ae04a98763a5726d22b7902dc64af5b388aa7374
yuri <ycs3@discente.ifpe.edu.br>
2026-02-25 19:32:54 -0300

    começo backend

---
+++ lumen-backend/src/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+import route from "./route";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", route);
+
+export default app;
